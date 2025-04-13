66989409b394ec51a1ac6b4770279e99a5e43de3
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-13 14:14:58 +0300

    hz

--- backend/parsing.py
+++ backend/parsing.py
@@ -1,6 +1,19 @@
-tok = 'y0__xDBvs7IBxjX-jYg_vft5xL__MMsXt5zlwFMr0IT2V-0kDWq0w    '
-
 import yadisk
+import asyncio
+from config import tok
+
 y = yadisk.YaDisk(token=tok)
+from sclib import SoundcloudAPI, Track, Playlist
+
+api = SoundcloudAPI()
+track = api.resolve('https://soundcloud.com/icegergert-685473693/casino')
+
+assert type(track) is Track
+
+filename = f'./{track.artist} - {track.title}.mp3'
+
+with open(filename, 'wb+') as file:
+    track.write_mp3_to(file)
 
 # y.upload("file1.txt", "/music/file1.txt")
+# Правильный формат (Python + psycopg2)
