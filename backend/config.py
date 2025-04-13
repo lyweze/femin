66a2dfa9e720cdc8bd22fcb81579c12efc4c6fff
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-13 21:17:52 +0300

    ОЭОЭФЭФФФФФЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫ ЭЭЫЭЫЭЫЫЫЫ ООО ААААААА  ААААААААААААААААААААААААААААААААА КАРТИНКЕ ЫЫЫ КАРТИНКЕ

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,2 @@
 tok = 'y0__xDBvs7IBxjX-jYg_vft5xL__MMsXt5zlwFMr0IT2V-0kDWq0w    '
+default_cover = "https://www.figma.com/design/aE3xlb4DeLcyroNqi6Aspi/Untitled?node-id=14-2&t=FtVyTqMfUACcPjHj-4"
--- backend/parsing_tracks_sync.py
+++ backend/parsing_tracks_sync.py
@@ -1,11 +1,25 @@
-import asyncio
 import io
-from tqdm.asyncio import tqdm_asyncio
+import requests
 import tempfile
 from sclib import SoundcloudAPI, Track, Playlist
-from sclib.asyncio import SoundcloudAPI, Playlist
 import yadisk
-from config import tok
+from config import tok, default_cover
+
+
+def save_cover(cover_url, save_to):
+
+    try:
+        if not cover_url:
+            cover_url = default_cover
+
+        response = requests.get(cover_url)
+        with tempfile.NamedTemporaryFile(suffix=".jpg", delete=True) as f:
+            f.write(response.content)
+            f.flush()
+            y.upload(f.name, save_to)
+    except Exception as e:
+        print(e)
+        return False
 
 def save_track(url): # скачка трека - оперативка
     track = api.resolve(url)
@@ -17,8 +31,10 @@ def save_track(url): # скачка трека - оперативка
 
     disk_path = f"/music/{track.artist} - {track.title}.mp3"
     y.upload(mp3_data, disk_path)
-
     mp3_data.close()
+
+    cover_path = f"music/covers/{track.artist} - {track.title}.jpg"
+    save_cover(track.artwork_url, cover_path)
     return 1
 
 def save_album(url):
@@ -34,10 +50,14 @@ def save_album(url):
 
             disk_path = f"{playlist_path}/{track.artist} - {track.title}.mp3"
             y.upload(f.name, disk_path)
+
+            cover_path = f"music/covers/{track.artist} - {track.title}.jpg"
+            save_cover(track.artwork_url, cover_path)
     return 1
 
 if __name__ == "__main__":
     y = yadisk.YaDisk(token=tok)
     api = SoundcloudAPI()
-    url = "https://soundcloud.com/illya-poludnenko/sets/serega-pirat"
-    save_track(url)
+    # url = "https://soundcloud.com/illya-poludnenko/sets/serega-pirat"
+    url = "https://soundcloud.com/user-544356536/sets/5uxxfdmldnek"
+    save_album(url)
