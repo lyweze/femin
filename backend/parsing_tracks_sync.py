3889599ff6986a5c9ef690a14858db7557659891
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-14 13:14:39 +0300

    hz

--- backend/parsing_tracks_sync.py
+++ backend/parsing_tracks_sync.py
@@ -2,26 +2,31 @@ import io
 import requests
 import tempfile
 from sclib import SoundcloudAPI, Track, Playlist
-import yadisk
-from config import tok, default_cover
 
+from backend.config import B2_KEY_ID, B2_APPLICATION_KEY, B2_BUCKET_NAME, default_cover
+import b2sdk.v2 as b2
 
-def save_cover(cover_url, save_to):
+def init_b2():
+    info = b2.InMemoryAccountInfo()
+    b2_api = b2.B2Api(info)
+    b2_api.authorize_account('production', B2_KEY_ID, B2_APPLICATION_KEY)
+    bucket = b2_api.get_bucket_by_name(B2_BUCKET_NAME)
+    return b2_api, bucket
+
+def save_cover(cover_url, b2_path, bucket):
 
     try:
         if not cover_url:
             cover_url = default_cover
 
         response = requests.get(cover_url)
-        with tempfile.NamedTemporaryFile(suffix=".jpg", delete=True) as f:
-            f.write(response.content)
-            f.flush()
-            y.upload(f.name, save_to)
+        bucket.upload_bytes(data_bytes=response.content, file_name=b2_path, content_type='image/jpeg')
+        return True
     except Exception as e:
         print(e)
         return False
 
-def save_track(url): # скачка трека - оперативка
+def save_track(url, b2_api, bucket): # скачка трека - оперативка
     track = api.resolve(url)
     assert type(track) is Track
 
@@ -29,35 +34,34 @@ def save_track(url): # скачка трека - оперативка
     track.write_mp3_to(mp3_data)
     mp3_data.seek(0)
 
-    disk_path = f"/music/{track.artist} - {track.title}.mp3"
-    y.upload(mp3_data, disk_path)
+    b2_path = f"/music/{track.artist} - {track.title}.mp3"
+    bucket.upload_bytes(mp3_data.read(), b2_path, content_type='audio/mp3')
     mp3_data.close()
 
     cover_path = f"music/covers/{track.artist} - {track.title}.jpg"
-    save_cover(track.artwork_url, cover_path)
+    save_cover(track.artwork_url, cover_path, bucket)
     return 1
 
-def save_album(url):
+def save_album(url, b2_api, bucket):
     playlist = api.resolve(url)
     assert type(playlist) is Playlist
 
     playlist_path = f"music/{playlist.title}"
-    if not y.exists(playlist_path):
-        y.mkdir(playlist_path)
+
     for track in playlist.tracks:
         with tempfile.NamedTemporaryFile(suffix=".mp3", delete=True) as f:
             track.write_mp3_to(f)
 
-            disk_path = f"{playlist_path}/{track.artist} - {track.title}.mp3"
-            y.upload(f.name, disk_path)
+            b2_path = f"{playlist_path}/{track.artist} - {track.title}.mp3"
+            bucket.upload_local_file(f.name, b2_path, content_type='audio/mp3')
 
             cover_path = f"music/covers/{track.artist} - {track.title}.jpg"
-            save_cover(track.artwork_url, cover_path)
+            save_cover(track.artwork_url, cover_path, bucket)
     return 1
 
 if __name__ == "__main__":
-    y = yadisk.YaDisk(token=tok)
+    b2_api, bucket = init_b2()
     api = SoundcloudAPI()
     # url = "https://soundcloud.com/illya-poludnenko/sets/serega-pirat"
     url = "https://soundcloud.com/user-544356536/sets/5uxxfdmldnek"
-    save_album(url)
+    save_album(url, b2_api, bucket)
