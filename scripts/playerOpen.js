92c9c0d416e69b88745ccb1bf2402d629888bae1
lyweze <borodulindenis60@gmail.com>
2025-04-15 21:55:05 +0300

    Альфа нахуй тест для ебаного караоке блять ЕБАААААААААААААААААть

--- scripts/playerOpen.js
+++ scripts/playerOpen.js
@@ -3,16 +3,18 @@ karaokeText.style.transform = 'scale(0.3)';
 
 function openPlayer(){
     if (isOpened === false){
-        footer.style.height = '100%';
+        footer.style.height = '80%';
         main.style.transform= 'scale(0.6)';
         main.style.filter = 'blur(20px)';
         karaokeText.style.opacity = '1';
         karaokeText.style.transform = 'scale(1)';
+        karaokeText.style.filter = '';
         karaokeText.style.height = '50%';
         isOpened = true;
     } else {
         karaokeText.style.opacity = '0';
         karaokeText.style.transform = 'scale(0.3)';
+        karaokeText.style.filter = 'blur(3px)';
         footer.style.height = '85px';
         main.style.transform= 'scale(1)';
         main.style.filter = 'blur(0)';
@@ -26,6 +28,8 @@ let k = 0;
 function audioText(){
     audio.addEventListener('playing', gg());
     function gg(){
+        let karaokeLi = document.getElementById('karaokeLi'); //Строчка текста песни
+        karaokeElement.textContent = '#karaokeText{ >li:nth-child(' + parseInt(audio.currentTime * 0.65 + 1).toString() + '){ color: #fff; font-size:60px; }}';
         karaokeLi.style.marginTop = (-64 * parseInt(audio.currentTime * 0.65)).toString() + 'px';
     }
 }
--- scripts/vars.js
+++ scripts/vars.js
@@ -14,10 +14,9 @@ let volume = document.getElementById('volume'); //Звук
 let rangeVolume = document.getElementById('rangeVolume'); //Изменение громкости
 let currenttrack = 0; //Текущий трек
 let footer = document.getElementById('footer'); //Весь футер
-// let footerDiv = document.getElementById('footer-div'); //Блок с элементами
 let main = document.getElementById('main'); //Main блок
 let karaokeText = document.getElementById('karaokeText'); //Текст песни (список)
-// let karaokeLi = document.getElementById('karaokeLi'); //Строчка текста песни
+let karaokeElement = document.getElementById('karaokeElement'); //Строка (верхнаяя) текста песни
 let isOpened = false;
 
 let ad = 'Еду сейчас на Москва-Сити; Долбоёбы, хуй сосите; Я только прилетел с Ростова,; завтра улетаю в Питер; Сисястых сук ебать их давно; профи — не любитель;Я сам себе предприниматель,; вижу лохов — наебать их;Чёрная рубашка, галстук;— я уже мужчина;Её туфли зацепили,; ведь они из крокодила;Первый день в Москве,; мы закрываем всю дрочильню;Я сейчас на чилле, так что,; шлюха, подрочи мне;'
