1f7f937cce59d1fd1bbc52b0b94c61b9d6000c4d
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-16 19:04:01 +0300

    updated with pylint, requirements.txt created

--- backend/music.sql
+++ backend/music.sql
@@ -1,4 +1,2 @@
-SELECT t.track_id, t.title, t.file_path AS mp3_url,
-       c.image_path AS cover_url
-FROM tracks t
-LEFT JOIN covers c ON t.track_id = c.track_id;
+ALTER SEQUENCE tracks_track_id_seq RESTART WITH 1;
+ALTER SEQUENCE covers_cover_id_seq RESTART WITH 1
