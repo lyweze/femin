0d19b33b89b8e0023da37d9615a770f19fb2ccc5
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-17 00:25:21 +0300

    ВСЕ РАБОТАЕТ НАДО ТОЛЬКО УБИРАТЬ НУЛИ ИЗ ТРЕКА ПАТАМУ ЧТО ОН ТОЖЕ РУГАЕТСЯ

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,13 @@
-tok = 'y0__xDBvs7IBxjX-jYg_vft5xL__MMsXt5zlwFMr0IT2V-0kDWq0w    '
-default_cover = "https://www.figma.com/design/aE3xlb4DeLcyroNqi6Aspi/Untitled?node-id=14-2&t=FtVyTqMfUACcPjHj-4"
+'''TOKEN_YA = 'y0__xDBvs7IBxjX-jYg_vft5xL__MMsXt5zlwFMr0IT2V-0kDWq0w    '
 B2_KEY_ID = "2b275676024d"
 B2_APPLICATION_KEY = "0034f8d5b5e6f5c68ff1f1229c5dd9b67f8186eefc"
-B2_BUCKET_NAME ="femin-music"
-DB_NAME = "music"
-DB_USER = "music"
+B2_BUCKET_NAME ="femin-musi'''
+
+SUPABASE_DB = "postgres"
+SUPABASE_USER = "postgres.arolhajzgbbvglokemwf"
+DEFAULT_COVER = "https://www.figma.com/design/aE3xlb4DeLcyroNqi6Aspi/Untitled?node-id=14-2&t=FtVyTqMfUACcPjHj-4"
+SUPABASE_URL = "https://arolhajzgbbvglokemwf.supabase.co"
+SUPABASE_HOST = "aws-0-eu-north-1.pooler.supabase.com"
+SUPABASE_PASSWORD = "12345_femin_hui225"
+SUPABASE_PORT = 5432
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyb2xoYWp6Z2Jidmdsb2tlbXdmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDgyMDI2MywiZXhwIjoyMDYwMzk2MjYzfQ.HjuliLH1hUKWJbBSRyCE1TfEs6quGmwSmyRchfnMvms"
