a425e9bd5dff1f6aa6d7e617e8e33680308885a0
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-17 19:51:13 +0300

    already dead 2 but api works

--- backend/api_femin.py
+++ backend/api_femin.py
@@ -2,13 +2,10 @@ import logging
 from typing import List
 
 import uvicorn
-from fastapi import FastAPI, HTTPException
-from fastapi.params import Depends
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-import psycopg2
-from psycopg2.extras import DictCursor
-from config import DB_NAME, DB_USER
-from parsing_tracks_sync import init_b2
+from supabase import create_client, Client
+from config import SUPABASE_URL, SUPABASE_KEY
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -16,59 +13,43 @@ app = FastAPI(title="Femin tracks API")
 
 
 class TrackResponse(BaseModel):
+    track_id: int
     title: str
     mp3_url: str
     cover_url: str
 
 
-b2_api, bucket = init_b2()
+def get_supabase() -> Client:
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    return supabase
 
 
-def get_db():
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, )
-    try:
-        yield conn
-        # возвращает генератор который приостанавливает функцию
-        # пока апи использует для запросов то есть необзятаельно закрывать
-    finally:
-        conn.close()
-
-
-def get_signed_url(file_name: str) -> str:
-    download_url = b2_api.get_download_url_for_file_name(bucket.name, file_name)
-    return download_url
 
 
 @app.get("/tracks", response_model=List[TrackResponse])
-async def get_tracks(conn: psycopg2.extensions.connection = Depends(get_db)):
+async def get_tracks(supabase: Client = Depends(get_supabase)):
     try:
-        cursor = conn.cursor(cursor_factory=DictCursor)
-        cursor.execute("""
-        SELECT t.track_id, t.title, t.file_path AS mp3_url, 
-                c.image_path AS cover_url
-        FROM tracks t
-        LEFT JOIN covers c ON t.track_id = c.track_id
-        """)
-        tracks = cursor.fetchall()
-        cursor.close()
-
+        response = supabase.table("tracks").select(
+            'track_id',
+            'title',
+            'file_path',
+            'covers(image_path)',
+        ).execute()
+        tracks = response.data
         track_response = []
         for track in tracks:
-            mp3_url = get_signed_url(track['mp3_url'])
-            cover_url = get_signed_url(track['cover_url'])
-            track_response.append(
-                TrackResponse(
-                    title=track["title"],
-                    mp3_url=mp3_url,
-                    cover_url=cover_url,
-                )
-            )
+            cover_url = track['covers'][0]['image_path'] if track['covers'] else ""
+            track_response.append(TrackResponse(
+                track_id=track['track_id'],
+                title=track['title'],
+                mp3_url=track['file_path'],
+                cover_url=cover_url,
+            ))
         logger.info(f"Got {len(track_response)} tracks")
         return track_response
     except Exception as e:
-        logger.error(f"Got exception {e}")
-        raise HTTPException(status_code=500, detail=str(e))
-
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="Something went wrong")
 
 @app.get("/")
 async def root():
@@ -77,4 +58,5 @@ async def root():
 
 
 if __name__ == "__main__":
+    supabase = get_supabase()
     uvicorn.run(app, host="0.0.0.0", port=8000)
