6a4d533519386a1df987f0983e160576e6217018
xyzxyz08 <xyzfortinder@gmail.com>
2025-04-18 11:12:53 +0300

    already fixed

--- backend/api_femin.py
+++ backend/api_femin.py
@@ -54,7 +54,7 @@ async def get_tracks(supabase: Client = Depends(get_supabase)):
 async def root():
 
     return {"message": "Welcome to Femin tracks API",
-            "tracks": "http://localhost:8000/tracks"}
+            "tracks": "feimn.onrender.com/tracks"}
 
 
 if __name__ == "__main__":
